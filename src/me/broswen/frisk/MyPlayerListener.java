7614be736a1c993daa93fe5e4684800d0096d2c2
Brad <broswen@gmail.com>
2013-10-24 21:55:19 -0500

    tried using friskstick :/

--- src/me/broswen/frisk/MyPlayerListener.java
+++ src/me/broswen/frisk/MyPlayerListener.java
@@ -7,11 +7,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -22,35 +26,21 @@ public class MyPlayerListener implements Listener{
 	public static Frisk plugin;
 	public static Economy econ = null;
 	
-	private boolean setupEconomy() {
-        if (plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = plugin.getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
 	/*
 	@EventHandler
-	public void onEntityDamage(EntityDamageByEntityEvent event){
-		Entity entity = event.getEntity();
-		Player player = (Player) event.getDamager();
+	public void onPlayerInteractEntity(PlayerInteractEntityEvent event){
+		Entity targetPlayer = event.getRightClicked();
+		Player player = event.getPlayer();
 		
-		if(entity instanceof Player){
-			Player targetPlayer = (Player) entity;
+		if(player.getItemInHand().getType() == Material.STICK){
 			
-			if(player.getItemInHand().getItemMeta().getDisplayName().equals("Frisk Stick") && player.getItemInHand().getType() == Material.STICK){
-				
-				event.setCancelled(true);
+			if(targetPlayer instanceof Player){
 				
 				if(player.hasPermission("frisk.friskstick")){
 					
-					if(!targetPlayer.hasPermission("frisk.exempt")){
+					if(!((Player) targetPlayer).hasPermission("frisk.exempt")){
 						
-						if(hasItem(targetPlayer, Material.INK_SACK, (short) 3) || hasItem(targetPlayer, Material.INK_SACK, (short) 2) || targetPlayer.getInventory().contains(Material.PUMPKIN_SEEDS) || targetPlayer.getInventory().contains(Material.MELON_SEEDS) || targetPlayer.getInventory().contains(Material.WHEAT) || targetPlayer.getInventory().contains(Material.SUGAR) || targetPlayer.getInventory().contains(Material.NETHER_STALK)){
+						if(hasItem((Player) targetPlayer, Material.INK_SACK, (short) 3) || hasItem((Player) targetPlayer, Material.INK_SACK, (short) 2) || ((HumanEntity) targetPlayer).getInventory().contains(Material.PUMPKIN_SEEDS) || ((HumanEntity) targetPlayer).getInventory().contains(Material.MELON_SEEDS) || ((HumanEntity) targetPlayer).getInventory().contains(Material.WHEAT) || ((HumanEntity) targetPlayer).getInventory().contains(Material.SUGAR) || ((HumanEntity) targetPlayer).getInventory().contains(Material.NETHER_STALK)){
 							
 							//checks if the targetplayer is in a vehicle
 							if(targetPlayer.isInsideVehicle()){
@@ -65,13 +55,13 @@ public class MyPlayerListener implements Listener{
 							Location jailLocation = new Location(Bukkit.getWorld(jailWorld), jailx, jaily, jailz);
 							
 							targetPlayer.teleport(jailLocation);
-							player.sendMessage(ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + " had drugs! You have recieved" + ChatColor.GREEN + " $1000!");
+							player.sendMessage(ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + " had drugs! You have recieved" + ChatColor.GREEN + " $1000!");
 							EconomyResponse r = econ.depositPlayer(player.getName(), 1000.0);
-							targetPlayer.sendMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught you with drugs!");
-							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught " + ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + " with drugs! he/she was sent to jail!");
-							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " recieved $1000 for jailing " + ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + "!");
+							((CommandSender) targetPlayer).sendMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught you with drugs!");
+							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught " + ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + " with drugs! he/she was sent to jail!");
+							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " recieved $1000 for jailing " + ChatColor.RED + ((Economy) targetPlayer).getName() + ChatColor.WHITE + "!");
 							
-						}else if(targetPlayer.getInventory().contains(Material.PUMPKIN) || targetPlayer.getInventory().contains(Material.SUGAR_CANE)){
+						}else if(((HumanEntity) targetPlayer).getInventory().contains(Material.PUMPKIN) || ((HumanEntity) targetPlayer).getInventory().contains(Material.SUGAR_CANE)){
 							
 							//checks if the targetplayer is in a vehicle
 							if(targetPlayer.isInsideVehicle()){
@@ -86,14 +76,14 @@ public class MyPlayerListener implements Listener{
 							Location jailLocation = new Location(Bukkit.getWorld(jailWorld), jailx, jaily, jailz);
 							
 							targetPlayer.teleport(jailLocation);
-							player.sendMessage(ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + "That player had paraphernalia! You have recieved" + ChatColor.GREEN + " $500!");
+							player.sendMessage(ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + "That player had paraphernalia! You have recieved" + ChatColor.GREEN + " $500!");
 							EconomyResponse r = econ.depositPlayer(player.getName(), 500.0);
-							targetPlayer.sendMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught you with paraphernalia!");
-							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught " + ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + " with paraphernalia! he/she was sent to jail!");
-							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " recieved" + ChatColor.GREEN + " $500" + ChatColor.WHITE + " for jailing " + ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + "!");
+							((CommandSender) targetPlayer).sendMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught you with paraphernalia!");
+							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " caught " + ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + " with paraphernalia! he/she was sent to jail!");
+							Bukkit.broadcastMessage(ChatColor.BLUE + player.getName() + ChatColor.WHITE + " recieved" + ChatColor.GREEN + " $500" + ChatColor.WHITE + " for jailing " + ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + "!");
 						
 						}else{
-							player.sendMessage(ChatColor.RED + targetPlayer.getName() + ChatColor.WHITE + " doesn't have anything! Stop frisking innocent people!");
+							player.sendMessage(ChatColor.RED + ((HumanEntity) targetPlayer).getName() + ChatColor.WHITE + " doesn't have anything! Stop frisking innocent people!");
 							player.addPotionEffect(new PotionEffect(PotionEffectType.HARM, 10, 0));
 							player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
 						}
@@ -103,14 +93,13 @@ public class MyPlayerListener implements Listener{
 					}
 					
 				}else{
-					player.sendMessage(ChatColor.GRAY + "You are not allowed to use that!");
+					player.sendMessage(ChatColor.GRAY + "You don't have permission!");
 				}
 				
 			}
+			
 		}
 	}
-	*/
-	
 	
 	public boolean hasItem(Player p, Material m, short s){
 	    Inventory inv = p.getInventory();
@@ -124,4 +113,5 @@ public class MyPlayerListener implements Listener{
 	    }
 	    return false;
 	}
+	*/
 }
